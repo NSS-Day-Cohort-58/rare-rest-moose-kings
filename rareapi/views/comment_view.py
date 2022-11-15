abaf4ebd06389903e0534ea4ef85a6f8258e0d5f
Scott Parks <swparks040@gmail.com>
2022-11-15 14:23:06 -0600

    small fix, in create and update comment_view

--- rareapi/views/comment_view.py
+++ rareapi/views/comment_view.py
@@ -3,6 +3,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from rareapi.models import Comment, Post, RareUser
+from datetime import date
 
 class CommentView(ViewSet):
     """Rare comments view"""
@@ -22,12 +23,12 @@ class CommentView(ViewSet):
 
     def create(self, request):
         post = Post.objects.get(pk=request.data['post'])
-        author = RareUser.objects.get(pk=request.data['rareuser'])
+        author = RareUser.objects.get(user=request.auth.user)
         comment = Comment.objects.create(
             post=post,
             author=author,
             content=request.data['content'],
-            created_on=request.data['created_on'],
+            created_on=date.today(),
             )
         serializer = CommentSerializer(comment)
         return Response(serializer.data, status = status.HTTP_201_CREATED)
@@ -36,7 +37,7 @@ class CommentView(ViewSet):
         comment = Comment.objects.get(pk=pk)
         comment.post = Post.objects.get(pk=request.data['post'])
         comment.author = RareUser.objects.get(user=request.auth.user)
-        comment.created_on = request.data['created_on']
+        comment.created_on = date.today()
         comment.content = request.data['content']
         comment.save()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
