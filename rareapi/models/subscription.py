1e7ffac2752b5b4fecee30ffc6102a1543798473
Brandon Herbison <herbisonbrandon@gmail.com>
2022-11-15 11:07:43 -0500

    created users

--- rareapi/models/subscription.py
+++ rareapi/models/subscription.py
@@ -3,7 +3,7 @@ from datetime import date
 
 class Subscription(models.Model):
 
-    follower = models.ForeignKey('RareUser', on_delete=models.CASCADE)
-    author = models.ForeignKey('RareUser', on_delete=models.CASCADE)
+    follower = models.ForeignKey('RareUser', on_delete=models.CASCADE, related_name='follower')
+    author = models.ForeignKey('RareUser', on_delete=models.CASCADE, related_name='author')
     created_on = date.today()
     ended_on = date.today()
--- rareapi/views/__init__.py
+++ rareapi/views/__init__.py
@@ -1,8 +1,8 @@
 from .auth import login_user, register_user
-from .category_view import CategoryView
-from .comment_view import CommentView
-from .post_view import PostView
-from .rare_user_view import RareUserView
-from .reaction_view import ReactionView
-from .subscription_view import SubscriptionView
-from .tag_view import TagView
+# from .category_view import CategoryView
+# from .comment_view import CommentView
+# from .post_view import PostView
+# from .rare_user_view import RareUserView
+# from .reaction_view import ReactionView
+# from .subscription_view import SubscriptionView
+# from .tag_view import TagView
