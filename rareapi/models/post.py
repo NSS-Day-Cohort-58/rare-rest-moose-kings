7cc4dcba7072195ac118d5d97e3fd744c3f08cea
Brandon Herbison <herbisonbrandon@gmail.com>
2022-11-15 15:33:41 -0500

    bug fixes

--- rareapi/models/post.py
+++ rareapi/models/post.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     user = models.ForeignKey('RareUser', on_delete=models.CASCADE)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
-    publication_date = date.today()
+    publication_date = models.DateField(default=date.today)
     image_url = models.TextField()
     content = models.TextField()
     approved = models.BooleanField()
