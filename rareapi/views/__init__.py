78976fbce0685289a8edaf60549ddf0aa294cdfa
Jonathan Woodard <jvw.226@gmail.com>
2022-11-16 09:41:48 -0600

    commit to pull from main

--- rareapi/views/__init__.py
+++ rareapi/views/__init__.py
@@ -1,7 +1,7 @@
 from .auth import login_user, register_user
 from .category_view import CategoryView
 from .comment_view import CommentView
-# from .post_view import PostView
+from .post_view import PostView
 # from .rare_user_view import RareUserView
 # from .reaction_view import ReactionView
 from .subscription_view import SubscriptionView
