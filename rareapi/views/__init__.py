1da6a6b8b2df4c5c293178079d93bcb6eb16fcc1
Matt <mattmartino54@gmail.com>
2022-11-16 09:34:22 -0600

    added sorting by ascending to tags and categories

--- rareapi/views/__init__.py
+++ rareapi/views/__init__.py
@@ -1,9 +1,7 @@
 from .auth import login_user, register_user
 from .category_view import CategoryView
 from .comment_view import CommentView
-# from .post_view import PostView
-# from .rare_user_view import RareUserView
-# from .reaction_view import ReactionView
+from .post_view import PostView
 from .subscription_view import SubscriptionView
 from .tag_view import TagView
 from .rare_user_view import RareUserView
